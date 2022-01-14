64d32588ae9d03afd0a4f7cc90e46599479be0a8
Ricardo Sawir <sawir.ricardo@gmail.com>
2022-01-14 08:47:33 +0700

    fixed typo

--- config/midtrans.php
+++ config/midtrans.php
@@ -3,8 +3,8 @@
 return [
     'server_key' => env('MIDTRANS_SERVER_KEY'),
     'client_key' => env('MIDTRANS_CLIENT_KEY'),
-    'sanbox_server_key' => env('MIDTRANS_SB_SERVER_KEY'),
-    'sanbox_client_key' => env('MIDTRANS_SB_CLIENT_KEY'),
+    'sandbox_server_key' => env('MIDTRANS_SB_SERVER_KEY'),
+    'sandbox_client_key' => env('MIDTRANS_SB_CLIENT_KEY'),
     'is_production' => env('MIDTRANS_IS_PRODUCTION', false),
     'is_sanitized' => true,
     'is_3ds' => true,
